74fb2b72e9d2633c33209ff774ae5efc444d3bf8
niloy12184 <muksedur45@gmail.com>
2023-08-17 15:45:13 +0600

    first commit

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ document.getElementById("btn-submit").addEventListener('click', function(){
     const email = emailField.value;
     const passwordField =document.getElementById("user-password");
     const password = passwordField.value;
-    if(email === 'muksedur45@gmail.com' && password ==='muksedur'){
+    if(email === 'mrn@gmail.com' && password ==='mrnbank'){
         window.location.href ="money.html";
     }else{
         alert('tore ami breckup korlam')
